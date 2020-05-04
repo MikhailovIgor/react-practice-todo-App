6536646e9710d0e8bfa2fb28091ee1bcdefffc86
MikhailovIgor <44831141+MikhailovIgor@users.noreply.github.com>
2020-05-04 14:42:35 +0300

    Update app.js
    made changes in "deleteItem" function

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -2,32 +2,33 @@ import React, {Component} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 import Service from "../service";
-import AppHeader from '../app-header'
+import AppHeader from '../app-header';
 import MainInput from '../main-input';
 import TodoList from '../todo-list';
 import BtnFilter from '../btn-filter';
 
 export default class App extends Component {
 
-    state = {
-        todoList : [
-            // this.createTodoItem('task1'),
-            // this.createTodoItem('task2'),
-            // this.createTodoItem('task3')
-        ],
-        filter: 'all' //all, active, completed
-    };
+    constructor() {
+        super();
+        this.state = {
+            todoList : [],
+            filter: 'all' //all, active, completed
+        };
+        console.log("App Constructor");
+    }
 
     componentDidMount() {
-       const test = new Service();
+        console.log("App Did Mount")
+       const service = new Service();
 
-       test.getData()
+        service.getData()
            .then(data => {
                this.setState(
                    {todoList: data}
                )
            })
-}
+    }
 
     createTodoItem(text) {
 
@@ -44,14 +45,15 @@ export default class App extends Component {
         const updatedTodoList = [
             ...this.state.todoList,
             newTask
-        ]
+        ];
 
-        this.setState({todoList: updatedTodoList})
+        this.setState({todoList: updatedTodoList});
     };
 
     onToggleDone = (id) => {
         this.setState(({todoList}) => {
             const idx = todoList.findIndex( el => el.id === id);
+            console.log(id);
             const oldItem = todoList[idx];
             const newItem = {...oldItem, checked: !oldItem.checked}
 
@@ -63,22 +65,29 @@ export default class App extends Component {
 
             return {
                 todoList: updatedTodoList
-            }
-        })
+            };
+        });
     };
 
     deleteItem = (id) => {
-        this.setState(({ todoList }) => {
-            const idx = todoList.findIndex( el => el.id === id);
-            const updatedTodoList = [
-                ...todoList.slice(0, idx),
-                ...todoList.slice(idx + 1)
-            ];
 
-            return {
-                todoList: updatedTodoList
-            }
-        });
+        const service = new Service();
+
+        service.deleteData(id).then(
+            this.setState(({ todoList }) => {
+                const idx = todoList.findIndex( el => el.id === id);
+                const updatedTodoList = [
+                    ...todoList.slice(0, idx),
+                    ...todoList.slice(idx + 1)
+                ];
+                console.log(updatedTodoList);
+
+                return {
+                    todoList: updatedTodoList
+                }
+            })
+        )
+
     };
 
     filterInFooter(items, filter) {
@@ -92,20 +101,21 @@ export default class App extends Component {
             default:
                 return items;
         }
-    }
+    };
 
     onFilterChange = (filter) => {
         this.setState({ filter })
     };
 
     render() {
+        console.log("App render")
         const {todoList, filter} = this.state;
         const todoCount = this.state.todoList.filter(el => el.checked === false).length;
 
         const visibleItems = this.filterInFooter(todoList, filter);
 
         return (
-            <div>
+            <>
                 <AppHeader />
                 <MainInput onItemAdded={this.addItem} />
                 <TodoList
@@ -118,7 +128,7 @@ export default class App extends Component {
                     filter={filter}
                     onFilterChange={this.onFilterChange}
                 />
-            </div>
+            </>
         );
     };
 }
