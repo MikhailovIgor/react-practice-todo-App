ff1908a3d252d42574d7421aecfdf223649165b8
MikhailovIgor <44831141+MikhailovIgor@users.noreply.github.com>
2020-05-04 14:44:25 +0300

    Update service.js
    added "deleteData" function

--- src/components/service.js
+++ src/components/service.js
@@ -1,11 +1,32 @@
 export default  class Service {
 
-    constructor(url = 'http://localhost:3001/myTodoList') {
+    constructor(url = 'http://localhost:3001/myTodoList', id) {
         this.url = url;
+        this.id = id;
     }
 
     async getData() {
         const resp = await fetch(this.url);
         return await resp.json();
     }
+
+    async deleteData(id) {
+        const resp = await fetch(`${this.url}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+        })
+            .then(() => console.log('фетч на удаление отработал', id))
+    }
+
+    // async changeData() {
+    //   const resp = await fetch(`${this.url}/${this.id}`, {
+    //       method: 'PATCH',
+    //       headers: {
+    //           'Content-Type': 'application/json;charset=utf-8'
+    //       },
+    //       body:
+    //   })
+    // }
 }
