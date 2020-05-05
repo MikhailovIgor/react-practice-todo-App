b99f68100a2f288370fa79dabeb3e4459d622645
Igor.Mikhailov <iomihailov@gmail.com>
2020-05-05 17:31:29 +0300

    practice in React JS

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -15,11 +15,11 @@ export default class App extends Component {
             todoList : [],
             filter: 'all' //all, active, completed
         };
-        console.log("App Constructor");
+        console.log("App: Constructor");
     }
 
     componentDidMount() {
-        console.log("App Did Mount")
+        console.log("App: componentDidMount")
        const service = new Service();
 
         service.getData()
@@ -42,31 +42,62 @@ export default class App extends Component {
     addItem = (text) => {
         const newTask = this.createTodoItem(text);
 
+        const url = 'http://localhost:3001/myTodoList'
+
         const updatedTodoList = [
             ...this.state.todoList,
             newTask
         ];
 
-        this.setState({todoList: updatedTodoList});
-    };
+        // отправляем на сервер. после успешного сохранения меняем в стейте
+        fetch(`${url}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(newTask)
+            })
+            .then( () => {
+                    this.setState(
+                        { todoList: updatedTodoList }
+                    )
+                }
+            )
+    }
 
     onToggleDone = (id) => {
-        this.setState(({todoList}) => {
-            const idx = todoList.findIndex( el => el.id === id);
-            console.log(id);
-            const oldItem = todoList[idx];
-            const newItem = {...oldItem, checked: !oldItem.checked}
 
-            const updatedTodoList = [
-                ...todoList.slice(0, idx),
-                newItem,
-                ...todoList.slice(idx + 1)
-            ];
+        const service = new Service();
+        service.changeData(true, id)
+            .then(() => {
+                this.setState(({todoList}) => {
+                    const idx = todoList.findIndex( el => el.id === id);
+                    console.log(id);
+                    const oldItem = todoList[idx];
+                    const newItem = {...oldItem, checked: !oldItem.checked}
 
-            return {
-                todoList: updatedTodoList
-            };
-        });
+                    const updatedTodoList = [
+                        ...todoList.slice(0, idx),
+                        newItem,
+                        ...todoList.slice(idx + 1)
+                    ];
+
+                    //вариант когда есть id
+
+                    // const updatedTodoList2 = todoList.map(task=>{
+                    //     if(task.id !== id){
+                    //         return task;
+                    //     }
+                    //
+                    //     return {...task, checked: !oldItem.checked};
+                    // });
+
+                    return {
+                        todoList: updatedTodoList
+                    };
+                })
+            }
+        )
     };
 
     deleteItem = (id) => {
@@ -108,7 +139,7 @@ export default class App extends Component {
     };
 
     render() {
-        console.log("App render")
+        console.log("App: render")
         const {todoList, filter} = this.state;
         const todoCount = this.state.todoList.filter(el => el.checked === false).length;
 
